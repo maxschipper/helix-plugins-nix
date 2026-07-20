4ca3aa9a0ecff7e53ec25570ccfd8be8e5ec0d78
Max S <maxschipper@noreply.codeberg.org>
2026-07-20 11:18:05 +0200

    helixPlugins.oil: 0-unstable-2026-07-06 -> 0-unstable-2026-07-20
    Diff: https://github.com/Ra77a3l3-jar/oil.hx/compare/da3af8e6bb405134818e35ea542d2ac90839c0bd...5e8fc997d862902b162eb96fb7914ae6ea02ae2b

--- pkgs/oil.nix
+++ pkgs/oil.nix
@@ -6,13 +6,13 @@
 }:
 buildHelixPlugin {
   pname = "oil";
-  version = "0-unstable-2026-07-06";
+  version = "0-unstable-2026-07-20";
 
   src = fetchFromGitHub {
     owner = "Ra77a3l3-jar";
     repo = "oil.hx";
-    rev = "da3af8e6bb405134818e35ea542d2ac90839c0bd";
-    hash = "sha256-Tu+Xn1uWQk4N4NjsJ9fEqWupEHMp4qhpiM8JkxvcNuo=";
+    rev = "5e8fc997d862902b162eb96fb7914ae6ea02ae2b";
+    hash = "sha256-IzTS2JNRLt7YGBecT8SVAIdCUf6ohvYFQfXQKiQoa90=";
   };
 
   pluginDependencies = [ notify ];
