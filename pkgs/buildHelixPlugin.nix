d73091a2d0b2664edbdbd3caba9faa34515db9f1
Max S <maxschipper@noreply.codeberg.org>
2026-07-22 11:37:09 +0200

    refactor(buildHelixPlugin) add support for the finalAttrs pattern this allows plugins to use finalAttrs instead of rec

--- pkgs/buildHelixPlugin.nix
+++ pkgs/buildHelixPlugin.nix
@@ -1,29 +1,40 @@
 { stdenv }:
 
-{
-  pname,
-  version,
-  src,
-  pluginDependencies ? [ ],
-  ...
-}@args:
-let
-  # only put args here that arent supposed to be in the mkDerivation set
-  # otherwise it breaks nix-update's ability to find the source location
-  extraArgs = removeAttrs args [
-    "pluginDependencies"
-  ];
+# {
+#   pname,
+#   version,
+#   src,
+#   pluginDependencies ? [ ],
+#   ...
+# }@args:
 
-  linkScmFiles = ''
-    find . -type f -name "*.scm" | while read -r file; do
-      # if the file is nested create the dir (e.g. src/)
-      mkdir -p "$out/$(dirname "$file")"
-      cp "$file" "$out/$file"
-    done
-  '';
+args:
 
-in
 stdenv.mkDerivation (
+  finalAttrs:
+  let
+    resolvedArgs = if builtins.isFunction args then args finalAttrs else args;
+
+    pname = resolvedArgs.pname;
+    version = resolvedArgs.version;
+    src = resolvedArgs.src;
+    pluginDependencies = resolvedArgs.pluginDependencies or [ ];
+
+    # only put args here that arent supposed to be in the mkDerivation set
+    # otherwise it breaks nix-update's ability to find the source location
+    extraArgs = removeAttrs resolvedArgs [
+      "pluginDependencies"
+    ];
+
+    linkScmFiles = ''
+      find . -type f -name "*.scm" | while read -r file; do
+        # if the file is nested create the dir (e.g. src/)
+        mkdir -p "$out/$(dirname "$file")"
+        cp "$file" "$out/$file"
+      done
+    '';
+
+  in
   {
     # this inherit is only for readability as these are overwritten by the merge
     # with extraArgs to preserve the source position for nix-update to work
@@ -41,5 +52,6 @@ stdenv.mkDerivation (
       ${linkScmFiles}
     '';
   }
+  # this also inherits pname version src
   // extraArgs
 )
--- pkgs/buildHelixPluginWithNative.nix
+++ pkgs/buildHelixPluginWithNative.nix
@@ -1,29 +1,42 @@
 { rustPlatform, lib }:
 
-{
-  pname,
-  version,
-  src,
-  cargoHash ? lib.fakeHash,
-  pluginDependencies ? [ ],
-  ...
-}@args:
-let
-  # only put args here that arent supposed to be in the mkDerivation set
-  # otherwise it breaks nix-update's ability to find the source location
-  extraArgs = removeAttrs args [
-    "pluginDependencies"
-  ];
+# {
+#   pname,
+#   version,
+#   src,
+#   cargoHash ? lib.fakeHash,
+#   pluginDependencies ? [ ],
+#   ...
+# }@args:
 
-  linkScmFiles = ''
-    find . -type f -name "*.scm" | while read -r file; do
-      # if the file is nested create the dir (e.g. src/)
-      mkdir -p "$out/$(dirname "$file")"
-      cp "$file" "$out/$file"
-    done
-  '';
-in
+args:
+
 rustPlatform.buildRustPackage (
+  finalAttrs:
+  let
+    resolvedArgs = if builtins.isFunction args then args finalAttrs else args;
+
+    # src
+    pname = resolvedArgs.pname;
+    version = resolvedArgs.version;
+    src = resolvedArgs.src;
+    cargoHash = resolvedArgs.cargoHash or lib.fakeHash;
+    pluginDependencies = resolvedArgs.pluginDependencies or [ ];
+
+    # everything that is supposed to be in the main buildRustPackage set needs
+    # to remain in extraArgs so nix-update can find the source location
+    extraArgs = removeAttrs resolvedArgs [
+      "pluginDependencies"
+    ];
+
+    linkScmFiles = ''
+      find . -type f -name "*.scm" | while read -r file; do
+        # if the file is nested create the dir (e.g. src/)
+        mkdir -p "$out/$(dirname "$file")"
+        cp "$file" "$out/$file"
+      done
+    '';
+  in
   {
     # this inherit is only for readability as these are overwritten by the merge
     # with extraArgs to preserve the source position for nix-update to work
