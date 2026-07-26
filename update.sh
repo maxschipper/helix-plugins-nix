a6716e1560d2eae7d108e7e8ac1ae8be54ae8392
Max S <maxschipper@noreply.codeberg.org>
2026-07-26 13:09:10 +0200

    refactor(update.sh): drop explicit default branch

--- update.sh
+++ update.sh
@@ -12,6 +12,10 @@ update() {
   echo
 }
 
+# --version=unstable allows tags with -alpha, -beta, -rc suffix or similar
+# --version=branch uses the latest commit of the default branch
+# you can also do --version=branch=BRANCH_NAME to use a different branch
+
 update helixPlugins.eval
 update helixPlugins.extend-sibling
 update helixPlugins.http
@@ -23,34 +27,34 @@ update helixPlugins.run-command
 update helixPlugins.ts-select
 update helixPlugins.ts-utils
 update helixPlugins.ui-utils
-update --version=branch=main helixPlugins.anchor
-update --version=branch=main helixPlugins.breadcrumbs
-update --version=branch=main helixPlugins.context
-update --version=branch=main helixPlugins.forest
-update --version=branch=main helixPlugins.glyph
-update --version=branch=main helixPlugins.hetex
-update --version=branch=main helixPlugins.hx-tmux-navigator
-update --version=branch=main helixPlugins.hxwiki
-update --version=branch=main helixPlugins.modeline
-# update --version=branch=main helixPlugins.moka # [todo] update again after https://github.com/mattwparas/helix/pull/129 is merged
-update --version=branch=main helixPlugins.notify
-update --version=branch=main helixPlugins.oil
-update --version=branch=main helixPlugins.scooter
-update --version=branch=main helixPlugins.show-keys
-update --version=branch=main helixPlugins.smooth-scroll
-update --version=branch=main helixPlugins.streal
-update --version=branch=main helixPlugins.switch
-update --version=branch=main helixPlugins.switcheroo
-update --version=branch=main helixPlugins.trail
-update --version=branch=main helixPlugins.wakatime
-update --version=branch=main helixPlugins.who
-update --version=branch=main helixPlugins.zen-mode
-update --version=branch=master helixPlugins.fake-warp
-update --version=branch=master helixPlugins.file-tree-hx
-update --version=branch=master helixPlugins.flash
-update --version=branch=master helixPlugins.helix-file-watcher
-update --version=branch=master helixPlugins.splash-hx
-update --version=branch=master helixPlugins.steel-pty
-update --version=branch=master helixPlugins.vim
-update --version=branch=trunk helixPlugins.microscope
+update --version=branch helixPlugins.anchor
+update --version=branch helixPlugins.breadcrumbs
+update --version=branch helixPlugins.context
+update --version=branch helixPlugins.fake-warp
+update --version=branch helixPlugins.file-tree-hx
+update --version=branch helixPlugins.flash
+update --version=branch helixPlugins.forest
+update --version=branch helixPlugins.glyph
+update --version=branch helixPlugins.helix-file-watcher
+update --version=branch helixPlugins.hetex
+update --version=branch helixPlugins.hx-tmux-navigator
+update --version=branch helixPlugins.hxwiki
+update --version=branch helixPlugins.microscope
+update --version=branch helixPlugins.modeline
+# update --version=branch helixPlugins.moka # [todo] update again after https://github.com/mattwparas/helix/pull/129 is merged
+update --version=branch helixPlugins.notify
+update --version=branch helixPlugins.oil
+update --version=branch helixPlugins.scooter
+update --version=branch helixPlugins.show-keys
+update --version=branch helixPlugins.smooth-scroll
+update --version=branch helixPlugins.splash-hx
+update --version=branch helixPlugins.steel-pty
+update --version=branch helixPlugins.streal
+update --version=branch helixPlugins.switch
+update --version=branch helixPlugins.switcheroo
+update --version=branch helixPlugins.trail
+update --version=branch helixPlugins.vim
+update --version=branch helixPlugins.wakatime
+update --version=branch helixPlugins.who
+update --version=branch helixPlugins.zen-mode
 update --version=unstable helixPlugins.juju # unstable because the tag has -alpha suffix
