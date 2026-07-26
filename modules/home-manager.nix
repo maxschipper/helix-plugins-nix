7e04a25ceb8f3adfba8d86cfa3273640f8f6a713
Max S <maxschipper@noreply.codeberg.org>
2026-07-26 15:13:44 +0200

    feat(modules): init home-manager module

---
+++ modules/home-manager.nix
@@ -0,0 +1,60 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  cfg = config.programs.helix;
+  inherit (lib.options) mkOption;
+
+  utils = import ./utils.nix { inherit lib pkgs; };
+
+  allPlugins = utils.flattenPlugins cfg.plugins;
+  nativePlugins = utils.getNativePlugins allPlugins;
+  nativeLibDrv = utils.mergeNativeLibs nativePlugins;
+
+  pluginLinks = builtins.listToAttrs (
+    map (drv: {
+      name = "steel/cogs/${drv.pluginName}";
+      value = {
+        source = drv;
+        force = true;
+      };
+    }) allPlugins
+  );
+
+  nativeLibLink = lib.optionalAttrs (nativeLibDrv != null) {
+    "steel/native" = {
+      source = nativeLibDrv;
+      force = true;
+    };
+  };
+
+in
+{
+  options.programs.helix = {
+    enable = lib.mkEnableOption "Helix editor with steel plugins";
+
+    package = mkOption {
+      type = lib.types.package;
+      default = pkgs.steelix;
+      description = "The helix package to wrap with steel plugins.";
+    };
+
+    plugins = mkOption {
+      type = lib.types.listOf lib.types.package;
+      default = [ ];
+      description = "List of Steel plugins to install for the Helix editor.";
+      example = lib.literalExpression "with pkgs.helixPlugins; [ oil notify scooter ];";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    # Install the base package into the user environment
+    home.packages = [ cfg.package ];
+
+    # Expose the links to Home Manager's XDG data directory manager
+    xdg.dataFile = pluginLinks // nativeLibLink;
+  };
+}
