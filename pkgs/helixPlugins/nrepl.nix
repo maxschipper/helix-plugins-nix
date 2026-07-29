8ca86238c31d1dd3def96f1082c5aae67d0769e6
Max S <maxschipper@noreply.codeberg.org>
2026-07-29 14:55:01 +0200

    nrepl: init at 0.6.0
    Diff: https://github.com/waddie/nrepl.hx/compare/v0.6.0...v0.6.0

---
+++ pkgs/helixPlugins/nrepl.nix
@@ -0,0 +1,37 @@
+{
+  buildHelixPluginWithNative,
+  fetchFromGitHub,
+  lib,
+
+  repl-ui,
+  run-command,
+  ui-utils,
+}:
+buildHelixPluginWithNative (finalAttrs: {
+  pname = "nrepl.hx";
+  version = "0.6.0";
+
+  src = fetchFromGitHub {
+    owner = "waddie";
+    repo = finalAttrs.pname;
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-L6bAAFaf0AEy4rKHdDYzMqWJnp4546JgCtfusachFhw=";
+  };
+
+  cargoHash = "sha256-nZq5MgHPcqZxtxos73hxPT2AV107hdapRXdPBs2TAZo=";
+
+  dependencies = [
+    repl-ui
+    run-command
+    ui-utils
+  ];
+
+  # [todo] tests
+
+  meta = {
+    description = "An nREPL client plugin for the Helix editor";
+    homepage = "https://github.com/waddie/nrepl.hx";
+    license = lib.licenses.agpl3Plus;
+    # maintainers = with lib.maintainers; [ ];
+  };
+})
