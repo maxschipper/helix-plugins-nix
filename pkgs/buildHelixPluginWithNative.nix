934f20330574888eb050f8d1ec30ef074238b504
Max S <maxschipper@noreply.codeberg.org>
2026-07-29 20:54:55 +0200

    feat(pkgs/buildHelixPluginWithNative): add support for steel tests

--- pkgs/buildHelixPluginWithNative.nix
+++ pkgs/buildHelixPluginWithNative.nix
@@ -1,4 +1,9 @@
-{ rustPlatform }:
+{
+  rustPlatform,
+  lib,
+  steel,
+  steel-test,
+}:
 
 args:
 rustPlatform.buildRustPackage (
@@ -16,12 +21,14 @@ rustPlatform.buildRustPackage (
     dependencies = resolvedArgs.dependencies or [ ]; # other plugins that should also be installed
     pluginName = resolvedArgs.pluginName or pname; # should be the cogs name (also used as the path in the modules)
     updateVersion = resolvedArgs.updateVersion or "stable"; # used for the update script; should be "stable" for tags, "unstable" for tags with "-alpha" suffix or similar, "branch" to follow the default branch, or "skip" if it should be skipped entirely
+    doSteelCheck = resolvedArgs.doSteelCheck or false;
 
     # only put args here that arent supposed to be merged into the mkDerivation set
     extraArgs = removeAttrs resolvedArgs [
       "pluginName"
       "dependencies"
       "updateVersion"
+      "doSteelCheck"
     ];
 
     linkScmFiles = ''
@@ -55,6 +62,49 @@ rustPlatform.buildRustPackage (
     ];
 
     cargoBuildFlags = [ "--lib" ];
+
+    nativeCheckInputs = lib.optionals doSteelCheck [ steel ];
+
+    postCheck = lib.optionalString doSteelCheck ''
+      echo "Running Steel tests..." 
+
+      export STEEL_HOME=$(mktemp -d)
+      mkdir -p $STEEL_HOME/cogs
+      ln -s ${steel-test} $STEEL_HOME/cogs/steel-test
+
+      # native lib setup
+      rm -rf target/release
+      mkdir -p $PWD/native
+      find target -maxdepth 3 -type f \( -name "*.so" -o -name "*.dylib" \) -exec cp {} $PWD/native/ \;
+
+      # link plugin deps
+      ${
+        (lib.concatMapStrings (dep: ''
+          ln -s ${dep} $STEEL_HOME/cogs/${dep.pluginName}
+        '') dependencies)
+      }
+
+      if [ -f tests/run-all.sh ]; then
+        echo "Found tests/run-all.sh"
+        sh tests/run-all.sh
+      elif [ -d tests ]; then
+        shopt -s nullglob; test_files=(tests/*.scm); shopt -u nullglob
+        
+        # fallback to running tests manually if there is no script
+        if [ ''${#test_files[@]} -gt 0 ]; then
+          echo "No tests/run-all.sh found"
+          echo "But found individual test files"
+          for f in "''${test_files[@]}"; do
+            echo "Running $f..."
+            steel "$f"
+          done
+        else
+          echo "no tests found"
+        fi
+      else
+        echo "no tests found"
+      fi
+    '';
 
     installPhase = ''
       mkdir -p $out $native
