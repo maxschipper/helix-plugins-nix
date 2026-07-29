1163023d31f5648b5ff66243dc80f0d789c0980b
Max S <maxschipper@noreply.codeberg.org>
2026-07-29 16:50:56 +0200

    feat(pkgs/buildHelixPlugin): fallback to running test files manually if no run-all script is found during checkPhase

--- pkgs/buildHelixPlugin.nix
+++ pkgs/buildHelixPlugin.nix
@@ -86,7 +86,26 @@ stdenvNoCC.mkDerivation (
 
       runHook preCheck
 
-      sh tests/run-all.sh
+      if [ -f tests/run-all.sh ]; then
+        echo "Found tests/run-all.sh"
+        sh tests/run-all.sh
+      elif [ -d tests ]; then
+        shopt -s nullglob; test_files=(tests/*.scm); shopt -u nullglob
+        
+        # fallback to running tests manually if there is no script
+        if [ ''${#test_files[@]} -gt 0 ]; then
+          echo "No tests/run-all.sh found"
+          echo "But found individual test files"
+          for f in "''${test_files[@]}"; do
+            echo "Running $f..."
+            steel "$f"
+          done
+        else
+          echo "no tests found"
+        fi
+      else
+        echo "no tests found"
+      fi
 
       runHook postCheck
     '';
