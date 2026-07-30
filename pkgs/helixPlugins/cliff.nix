9f82885724b7e252459cb1172f333c4911ff79b1
Max S <maxschipper@noreply.codeberg.org>
2026-07-30 19:30:58 +0200

    cliff: init at 0-unstable-2026-07-29
    https://github.com/Ra77a3l3-jar/cliff.hx

---
+++ pkgs/helixPlugins/cliff.nix
@@ -0,0 +1,24 @@
+{
+  buildHelixPlugin,
+  fetchFromGitHub,
+  lib,
+}:
+buildHelixPlugin (finalAttrs: {
+  pname = "cliff.hx";
+  version = "0-unstable-2026-07-29";
+  updateVersion = "branch";
+
+  src = fetchFromGitHub {
+    owner = "Ra77a3l3-jar";
+    repo = finalAttrs.pname;
+    rev = "a188886aa5625e4da01b9714033a035bd5f6fed8";
+    hash = "sha256-oecsWoSoTIQvJohMKhylxEdanfqHgsihQTiLjNYXwvo=";
+  };
+
+  meta = {
+    description = "Tree-sitter code folding for Helix (Steel plugin) — collapse functions, loops, and blocks to a single line, without ever touching your file.";
+    homepage = "https://github.com/Ra77a3l3-jar/cliff.hx";
+    license = lib.licenses.mit;
+    # maintainers = with lib.maintainers; [ ];
+  };
+})
