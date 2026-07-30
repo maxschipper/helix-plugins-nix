76a30ae4e3671f3619846cef1cb16590a23e2094
Max S <maxschipper@noreply.codeberg.org>
2026-07-30 18:52:31 +0200

    lsp-picker: init at 0-unstable-2026-07-02
    https://github.com/brnosouza/lsp-picker.hx

---
+++ pkgs/helixPlugins/lsp-picker.nix
@@ -0,0 +1,24 @@
+{
+  buildHelixPlugin,
+  fetchFromGitHub,
+  lib,
+}:
+buildHelixPlugin (finalAttrs: {
+  pname = "lsp-picker.hx";
+  version = "0-unstable-2026-07-02";
+  pluginName = "lsp-picker";
+
+  src = fetchFromGitHub {
+    owner = "brnosouza";
+    repo = finalAttrs.pname;
+    rev = "61ce6e3a90c2725ca2fc7539b9d76d83533e7c59";
+    hash = "sha256-mecqTw37Dj+cf3SPdhbFH6y21hRP94rMsdPjMgOPPDQ=";
+  };
+
+  meta = {
+    description = "A Helix plugin (Steel/Scheme) that opens a floating picker to manage LSP servers attached to the current buffer.";
+    homepage = "https://github.com/brnosouza/lsp-picker.hx";
+    license = lib.licenses.mit;
+    # maintainers = with lib.maintainers; [ ];
+  };
+})
