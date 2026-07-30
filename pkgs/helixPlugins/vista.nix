fe94ade6e004d65bc3b7292d5c044bd1780054b6
Max S <maxschipper@noreply.codeberg.org>
2026-07-30 19:30:58 +0200

    vista: init at 0-unstable-2026-07-30
    https://github.com/Ra77a3l3-jar/vista.hx

---
+++ pkgs/helixPlugins/vista.nix
@@ -0,0 +1,24 @@
+{
+  buildHelixPlugin,
+  fetchFromGitHub,
+  lib,
+}:
+buildHelixPlugin (finalAttrs: {
+  pname = "vista.hx";
+  version = "0-unstable-2026-07-30";
+  updateVersion = "branch";
+
+  src = fetchFromGitHub {
+    owner = "Ra77a3l3-jar";
+    repo = finalAttrs.pname;
+    rev = "fd53b347641c872c73acd563c07c60eb7316ae2b";
+    hash = "sha256-K2M/a6CpWjtaYaGBOYSZZ2ublHSslb7zkvelHbCsCUo=";
+  };
+
+  meta = {
+    description = "In-editor markdown rendering for Helix";
+    homepage = "https://github.com/Ra77a3l3-jar/vista.hx";
+    license = lib.licenses.mit;
+    # maintainers = with lib.maintainers; [ ];
+  };
+})
