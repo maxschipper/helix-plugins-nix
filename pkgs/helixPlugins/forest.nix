90cd99fb607344e7c24cb88ebd018e899b6ecc1b
Max S <maxschipper@noreply.codeberg.org>
2026-08-03 01:04:56 +0200

    forest: 0-unstable-2026-08-02 -> 0-unstable-2026-08-02
    Diff: https://github.com/Ra77a3l3-jar/forest.hx/compare/d6cc252930d7532c2f69710a18e47e61c02cf86b...c487956a8f002813fe44ae2a30fffe1859fcc206

--- pkgs/helixPlugins/forest.nix
+++ pkgs/helixPlugins/forest.nix
@@ -15,8 +15,8 @@ buildHelixPlugin (finalAttrs: {
   src = fetchFromGitHub {
     owner = "Ra77a3l3-jar";
     repo = finalAttrs.pname;
-    rev = "d6cc252930d7532c2f69710a18e47e61c02cf86b";
-    hash = "sha256-O8rs83uvXfMQrb92C+DmI9euK8VLHYLo4meFN539R2s=";
+    rev = "c487956a8f002813fe44ae2a30fffe1859fcc206";
+    hash = "sha256-b3O7+R5F9TQ0F5m8FYe6ka8mgCLq3IGw4d1Kdh+Y1l0=";
   };
 
   pluginDependencies = [
