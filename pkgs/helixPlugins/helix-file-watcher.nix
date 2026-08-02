71f949002f84116fb2f44275b84cfa39b5bdf9f9
Max S <maxschipper@noreply.codeberg.org>
2026-08-03 01:05:13 +0200

    helix-file-watcher: 0-unstable-2026-05-21 -> 0-unstable-2026-08-02
    Diff: https://github.com/mattwparas/helix-file-watcher/compare/e118b7552ec7697c560a24b48880c92d6aa4476e...ea1ad0dfc1f5f806eed837baf01ef0263cff1be0

--- pkgs/helixPlugins/helix-file-watcher.nix
+++ pkgs/helixPlugins/helix-file-watcher.nix
@@ -5,14 +5,14 @@
 }:
 buildHelixPluginWithNative (finalAttrs: {
   pname = "helix-file-watcher";
-  version = "0-unstable-2026-05-21";
+  version = "0-unstable-2026-08-02";
   updateVersion = "branch";
 
   src = fetchFromGitHub {
     owner = "mattwparas";
     repo = finalAttrs.pname;
-    rev = "e118b7552ec7697c560a24b48880c92d6aa4476e";
-    hash = "sha256-AvUihtnJtVZ6cLJJrNzhTmt/ZT1lZzprCRbuAfbzRc0=";
+    rev = "ea1ad0dfc1f5f806eed837baf01ef0263cff1be0";
+    hash = "sha256-O8qkQX+yYjSCtnZ59jfVJdPYc7XTvMpBw3gXtWtjfAk=";
   };
 
   cargoHash = "sha256-RhxKQSydcY48/aWZGPbJe6pFrKptynMV35BQSD16tXo=";
