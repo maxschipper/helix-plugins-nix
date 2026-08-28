c4495b5cb8f20854c072c95f9ed0281285f8e60f
paige <paigely@tuta.io>
2026-08-28 10:48:43 +0200

    presence: init at 0.1.0
    adds a package for (my fork of) `presence.hx` which is a plugin that adds Discord rich presence for Helix

---
+++ pkgs/helixPlugins/presence.nix
@@ -0,0 +1,26 @@
+{
+  buildHelixPluginWithNative,
+  fetchFromCodeberg,
+  lib,
+}:
+buildHelixPluginWithNative (finalAttrs: {
+  pname = "presence.hx";
+  cogName = "helix-discord-rpc";
+  version = "0.1.0";
+
+  src = fetchFromCodeberg {
+    owner = "paige";
+    repo = finalAttrs.pname;
+    rev = "ae7268372898e33d418fc613de5d473964e40f8c";
+    hash = "sha256-L03AaAVzVLDndH7VSXP+6AaZJdU+4YoVloTpygfYw+o=";
+  };
+
+  cargoHash = "sha256-PEgm8O3YCpHuQLzEhR1FimJNvC4WBWw2K92ZTQ4qS7M=";
+
+  meta = {
+    description = "discord rich presence for helix";
+    homepage = "https://codeberg.org/paige/presence.hx";
+    license = lib.licenses.mit;
+    # maintainers = with lib.maintainers; [ ];
+  };
+})
