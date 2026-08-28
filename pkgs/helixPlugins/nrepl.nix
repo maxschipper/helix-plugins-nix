b731c3874d2496af22b3b88204e4536a82b78e26
Max S <maxschipper@noreply.codeberg.org>
2026-08-28 11:04:51 +0200

    nrepl: 0.6.2 -> 0.6.4
    Diff: https://github.com/waddie/nrepl.hx/compare/v0.6.2...v0.6.4

--- pkgs/helixPlugins/nrepl.nix
+++ pkgs/helixPlugins/nrepl.nix
@@ -9,16 +9,16 @@
 }:
 buildHelixPluginWithNative (finalAttrs: {
   pname = "nrepl.hx";
-  version = "0.6.2";
+  version = "0.6.4";
 
   src = fetchFromGitHub {
     owner = "waddie";
     repo = finalAttrs.pname;
     tag = "v${finalAttrs.version}";
-    hash = "sha256-GNe24bQoMR2AKnoEhdoIjJbv9uBOx5lLw6olbhUScy4=";
+    hash = "sha256-t8NWaRZiE1G5xrTfXX47dkbKlWJYQFt9VMLXFBe2BU8=";
   };
 
-  cargoHash = "sha256-F/UgEFSpkpfqdlMB3J1/ykG+t8HqhmuD/aRFEv9NPbc=";
+  cargoHash = "sha256-qBSu2t2Ji35Jy/VhBNGFQwbint94MnQkY6jFDVtb7Sc=";
 
   pluginDependencies = [
     repl-ui
