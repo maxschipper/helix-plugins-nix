558d16cd99c8f7c71e805f04da2cab3467ed2e27
Max S <maxschipper@noreply.codeberg.org>
2026-09-16 13:35:57 +0200

    codesnap: init at 0.4.0

---
+++ pkgs/helixPlugins/codesnap.nix
@@ -0,0 +1,31 @@
+# you also need to add pkgs.silicon to your environment.systemPackages
+{
+  buildHelixPlugin,
+  fetchFromGitHub,
+  lib,
+
+  ui-utils,
+}:
+buildHelixPlugin (finalAttrs: {
+  pname = "codesnap.hx";
+  version = "0.4.0";
+  cogName = "codesnap";
+
+  src = fetchFromGitHub {
+    owner = "Vyrnexis";
+    repo = finalAttrs.pname;
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-9KB4ReGnGlStFijg+JLJEUgE+591uI/QC2M2CeMJuEA=";
+  };
+
+  pluginDependencies = [ ui-utils ];
+
+  doSteelCheck = true;
+
+  meta = {
+    description = "Generate code snippets from your Helix visual selections. Powered by Silicon and the Steel plugin system.";
+    homepage = "https://github.com/Vyrnexis/codesnap.hx";
+    license = lib.licenses.mit;
+    # maintainers = with lib.maintainers; [ ];
+  };
+})
