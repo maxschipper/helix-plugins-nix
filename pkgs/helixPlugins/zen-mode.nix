84168a15d7a26e45179f327382cddcb2cf16a1a4
Max S <maxschipper@noreply.codeberg.org>
2026-09-17 15:29:11 +0200

    zen-mode: 0-unstable-2026-09-02 -> 0-unstable-2026-09-17
    Diff: https://github.com/notnmeyer/zen-mode.hx/compare/f9fca83713c00e17ecad21edb6ac17c48c8de4bd...ce69db0d6f2ebc2dc9dbcd8bd089d4a29f1c773a

--- pkgs/helixPlugins/zen-mode.nix
+++ pkgs/helixPlugins/zen-mode.nix
@@ -5,15 +5,15 @@
 }:
 buildHelixPlugin (finalAttrs: {
   pname = "zen-mode.hx";
-  version = "0-unstable-2026-09-02";
+  version = "0-unstable-2026-09-17";
   cogName = "zen-mode";
   updateVersion = "branch";
 
   src = fetchFromGitHub {
     owner = "notnmeyer";
     repo = finalAttrs.pname;
-    rev = "f9fca83713c00e17ecad21edb6ac17c48c8de4bd";
-    hash = "sha256-yE0sHytryj+nguEiEUvDbzpf42KKxWa0ZtuK2SNFSpM=";
+    rev = "ce69db0d6f2ebc2dc9dbcd8bd089d4a29f1c773a";
+    hash = "sha256-Ne244Ekf1PBBsnx6Uq1hA4nvdTaQ4KDvjGhrdcWQw30=";
   };
 
   meta = {
